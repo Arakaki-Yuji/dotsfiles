38b33a59a9e6ad8726b2f558b40968c264b4d128
新垣雄志 <yuji-a@aragakiyuushinoMacBook-Pro.local>
2017-07-31 15:42:05 +0900

    add .tmux.conf

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ DOT_FILES=(.emacs.d/init.el .emacs.d/Cask)
 mkdir ~/.emacs.d
 ln -s $HOME/dotsfiles/.emacs.d/init.el $HOME/.emacs.d/init.el
 ln -s $HOME/dotsfiles/.emacs.d/Cask $HOME/.emacs.d/Cask
+ln -s $HOME/dotsfiles/.tmux.conf $HOME/.tmux.conf
